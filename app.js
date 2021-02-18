a192ee65842dda992a018d4ecd24677001fd88a4
iymeko <erica@iymeko.com>
2021-02-18 08:27:50 -0600

    switch to webpack; use github-pages branch

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Welcome to setting up Node.js project tutorial!'))
-
-app.listen(port, () => console.log(`Application listening on port ${port}!`))
-
-app.use(express.static(__dirname))
